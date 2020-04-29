20bd6f01ae0745dc25fce30b566cd2419adf752a
ihimanshubhutani <38641744+ihimanshubhutani@users.noreply.github.com>
2020-04-29 08:30:15 +0530

    whiteboard ready

--- drawer.js
+++ drawer.js
@@ -7,3 +7,34 @@ function resize() {
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
 }
+
+window.addEventListener("resize", resize);
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
+
+
+var pos = { x: 0, y: 0 };
+
+function setPosition(e) {
+  pos.x = e.clientX;
+  pos.y = e.clientY;
+}
+
+function draw(e) {
+  if (e.buttons !== 1) return;
+
+  var color = document.getElementById("hex").value;
+
+  ctx.beginPath();
+
+  ctx.lineWidth = 20;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = color;
+
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+
+  ctx.stroke();
+}
